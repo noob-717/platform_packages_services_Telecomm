7b00977c608574ee59c63391fa3ada063b93e430
Danesh M <daneshm90@gmail.com>
2015-09-25 16:38:47 -0700

    Telecomm : Allow multiple default dialers [1/2] *also Avoid npe for devices that don't have dialer package installed.
    Pure Edits:
    Modified this for google dialer support without having to modify the apks every release

--- src/com/android/server/telecom/TelecomServiceImpl.java
+++ src/com/android/server/telecom/TelecomServiceImpl.java
@@ -598,10 +598,7 @@ public class TelecomServiceImpl {
             try {
                 Log.startSession("TSI.gDPA");
                 // No need to synchronize
-                Resources resources = mContext.getResources();
-                return new ComponentName(
-                        resources.getString(R.string.ui_default_package),
-                        resources.getString(R.string.dialer_default_class));
+                return TelephonyUtil.getDialerComponentName(mContext);
             } finally {
                 Log.endSession();
             }
@@ -635,7 +632,12 @@ public class TelecomServiceImpl {
         public String getSystemDialerPackage() {
             try {
                 Log.startSession("TSI.gSDP");
-                return mContext.getResources().getString(R.string.ui_default_package);
+                String dialerPackage = null;
+                ComponentName component = TelephonyUtil.getDialerComponentName(mContext);
+                if (component != null) {
+                    dialerPackage = component.getPackageName();
+                }
+                return dialerPackage;
             } finally {
                 Log.endSession();
             }
